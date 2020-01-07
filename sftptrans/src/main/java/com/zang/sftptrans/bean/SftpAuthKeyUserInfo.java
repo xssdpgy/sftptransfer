5efc097b0ebb64680ce6e9299281d4229c61299a
xssdpgy <2234501359@qq.com>
2020-01-07 22:39:09 +0800

    sftp连接实体 ssh登录口令

---
+++ sftptrans/src/main/java/com/zang/sftptrans/bean/SftpAuthKeyUserInfo.java
@@ -0,0 +1,49 @@
+package com.zang.sftptrans.bean;
+
+import com.jcraft.jsch.UserInfo;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * SSH private key passphrase info
+ *
+ * @author zangjf
+ * @version zangjf: SftpAuthKeyUserInfo.java,v1.0 2020/1/7 20:59 zangjf Exp $$
+ * @since 1.0
+ */
+@Slf4j
+@AllArgsConstructor
+public class SftpAuthKeyUserInfo implements UserInfo{
+    //SSH private key passphrase
+    private String passphrase;
+
+    @Override
+    public String getPassphrase() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean promptPassword(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean promptPassphrase(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean promptYesNo(String s) {
+        return false;
+    }
+
+    @Override
+    public void showMessage(String s) {
+        log.info("SSH Message is: {}",s);
+    }
+}
